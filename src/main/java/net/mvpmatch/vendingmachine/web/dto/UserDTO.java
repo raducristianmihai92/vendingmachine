d8688312e60c0159ca16bd22d658639d62991c0e
raducristianmihai92 <raducristianmihai92@gmail.com>
2021-11-23 17:45:35 +0200

    remove pass

--- src/main/java/net/mvpmatch/vendingmachine/web/dto/UserDTO.java
+++ src/main/java/net/mvpmatch/vendingmachine/web/dto/UserDTO.java
@@ -10,12 +10,6 @@ public class UserDTO implements Serializable {
 
     private Integer id;
     private String userName;
-    private String password;
     private Integer deposit;
     private String role;
-
-    @JsonIgnore
-    public String getPassword() {
-        return password;
-    }
 }
